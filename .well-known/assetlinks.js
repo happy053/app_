f44a939d4d9bb528b1f224b3ab15537ce923e9cf
happy053 <wjdals053@gmail.com>
2025-04-21 10:41:10 +0900

    test

--- .well-known/assetlinks.js
+++ .well-known/assetlinks.js
@@ -1,9 +1,11 @@
-[{
+[
+    {
     "relation": ["delegate_permission/common.handle_all_urls"],
     "target": {
         "namespace": "android_app",
         "package_name": "tank.tankapp",
         "sha256_cert_fingerprints":
             ["BB:B6:13:61:93:19:93:7E:D3:9F:50:18:9A:9D:A2:EE:29:26:D3:64:4D:78:B3:4F:B9:47:7F:8D:90:2B:C6:DF"]
+        }
     }
-}]
+]
